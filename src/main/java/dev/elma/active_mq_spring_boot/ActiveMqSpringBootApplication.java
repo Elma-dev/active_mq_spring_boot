e1d33069b8b77044edab30d224002aa1752a672d
Elma-dev <67378945+Elma-dev@users.noreply.github.com>
2023-09-28 15:03:15 +0100

    Initial commit

---
+++ src/main/java/dev/elma/active_mq_spring_boot/ActiveMqSpringBootApplication.java
@@ -0,0 +1,13 @@
+package dev.elma.active_mq_spring_boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActiveMqSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ActiveMqSpringBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/elma/active_mq_spring_boot/ActiveMqSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.elma.active_mq_spring_boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActiveMqSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
